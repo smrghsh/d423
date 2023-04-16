53f7ee70b3427d8bb1a32d2714a2fd54dc19f96e
Samir Ghosh <hello@samir.tech>
2023-04-16 16:12:48 -0700

    add: initial XR template

---
+++ src/Experience/World/World.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three'
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import Floor from './Floor.js'
+import Spectra from './Spectra.js'
+import Stars from './Stars.js'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        this.floor = new Floor()
+        // this.circles = new Circles()
+        // this.hypercube = new Hypercube()
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            console.log('resources ready')
+            // this.test = new Test()
+            this.stars = new Stars()
+            this.spectra = new Spectra()
+            // this.sushi = new Sushi()
+            
+            this.environment = new Environment()
+        })
+    }
+    update() {
+        // this.circles.update()
+    }
+}
